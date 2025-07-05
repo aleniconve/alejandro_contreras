c1d94eb7e367406fe063c94a5fd36c535b04d260
Alejandro Contreras <contrerasx@gmail.com>
2025-07-05 15:21:24 +0200

    credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,19 +76,13 @@ Rails.application.configure do
 
 
 # Email via SMTP
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
-config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "gmail.com",
-  user_name: Rails.application.credentials.dig(:gmail, :user_name),
-  password: Rails.application.credentials.dig(:gmail, :app_password),
-  authentication: "plain",
-  enable_starttls_auto: true
-}
-
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               "gmail.com",
+    user_name:            Rails.application.credentials.email_user,
+    password:             Rails.application.credentials.email_password,
+    authentication:       "plain",
+    enable_starttls_auto: true
+  }
 end
